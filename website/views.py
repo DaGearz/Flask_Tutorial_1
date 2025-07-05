bad3f26a78109be0fda7502e5a630192b1f234f0
DaGearz <twill003@gmail.com>
2025-07-05 15:23:20 -0700

    initial commit

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for, session
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Welcome to Tim_01</h1>"
+    return render_template("home.html", boolean = True)
 
